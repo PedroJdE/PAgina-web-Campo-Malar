0cc8d9b629976a54ac65c3509bd4e486e031bc4e
pedro_jde <pedrojuliandeelias@gmail.com>
2026-07-30 19:27:24 -0300

    cors edit

--- backend/backend-reservas/server.js
+++ backend/backend-reservas/server.js
@@ -37,7 +37,7 @@ app.use(async (req, res, next) => {
 });
 
 const allowedOrigins = [
-    "https://campo-malar.vercel.app",
+    "https://pagina-web-campo-malar.vercel.app",
     "http://127.0.0.1:5500",
     "http://localhost:5500"
 ];
